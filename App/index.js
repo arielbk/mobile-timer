efa4a00114576c7b0a06c45c1e24670e57365d22
arielbk <buchwald.kearney@gmail.com>
2020-03-28 17:28:00 +0100

    UX tweaks

--- App/index.js
+++ App/index.js
@@ -1,17 +1,17 @@
 // initial guidance from https://dev.to/nabendu82/simple-timer-app-with-react-native-434i
 
 import React, { useState, useReducer, useRef, useEffect } from 'react';
-import { StyleSheet, Text, View, StatusBar, TouchableOpacity, Dimensions } from 'react-native';
+import { StyleSheet, Text, View, StatusBar, TouchableOpacity, Dimensions, Platform } from 'react-native';
 const screen = Dimensions.get('window');
 
 const formatMillis = time => {
   const mins = Math.floor(time / 1000 / 60);
   const secs = Math.floor(time / 1000 - mins * 60);
-  const millis = time - mins * 60 * 1000 - secs * 1000;
+  const centis = Math.floor((time - mins * 60 * 1000 - secs * 1000) / 10);
   return {
     minutes: String(mins).padStart(2, '0'),
     seconds: String(secs).padStart(2, '0'),
-    milliseconds: String(millis).padStart(3, '0')
+    centiseconds: String(centis).padStart(2, '0')
   }
 }
 
@@ -66,13 +66,13 @@ export default function App() {
     if (state.isActive) {
       interval.current = setInterval(() => {
         setMillis(state.millisSaved + new Date().getTime() - state.lastStarted)
-      }, 1);
+      }, 10);
     } else {
       clearInterval(interval.current);
     }
   }, [state.isActive]);
 
-  const {minutes, seconds, milliseconds} = formatMillis(millis);
+  const {minutes, seconds, centiseconds} = formatMillis(millis);
 
   return (
     <View style={styles.container}>
@@ -80,7 +80,7 @@ export default function App() {
       <StatusBar barStyle="light-content" />
 
       <Text style={styles.timerText}>
-        {minutes}:{seconds}:{milliseconds}
+        {minutes}:{seconds}.{centiseconds}
       </Text>
 
       <TouchableOpacity onPress={handleReset} style={[styles.buttonReset]}>
@@ -89,7 +89,13 @@ export default function App() {
         </Text>
       </TouchableOpacity>
 
-      <TouchableOpacity onPress={state.isActive ? handlePause : state.millisSaved ? handleResume : handleStart} style={styles.button}>
+      <TouchableOpacity
+        onPress={state.isActive
+          ? handlePause
+          : state.millisSaved 
+            ? handleResume 
+            : handleStart}
+        style={{...styles.button, borderColor: state.isActive ? '#314248' : '#B4D6E3' }}>
         <Text style={styles.buttonText}>
           {state.isActive ? 'Pause' : state.millisSaved ? 'Resume' : 'Start'}
         </Text>
@@ -108,7 +114,6 @@ const styles = StyleSheet.create({
   },
   button: {
     borderWidth: 6,
-    borderColor: '#B4D6E3',
     width: screen.width / 2,
     height: screen.width / 2,
     borderRadius: screen.width / 2,
@@ -122,10 +127,12 @@ const styles = StyleSheet.create({
   timerText: {
     color: '#fff',
     fontSize: 80,
+    marginTop: screen.height / 7,
     marginBottom: 20,
+    fontVariant: ['tabular-nums'],
   },
   buttonReset: {
-    marginBottom: 120,
+    marginBottom: 180,
   },
   buttonTextReset: {
     fontSize: 30,
